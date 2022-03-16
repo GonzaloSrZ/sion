ff9cefbcdbe6e5bd719b63e602b6b279637c30d4
GonzaloSrZ <gon.srz@gmail.com>
2022-03-15 23:34:58 -0300

    tablaclientes

--- app/Http/Livewire/TablaClientes.php
+++ app/Http/Livewire/TablaClientes.php
@@ -2,14 +2,46 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\t_clientes;
 use Livewire\Component;
-
+use Livewire\WithPagination;
 class TablaClientes extends Component
 {
-    
-    
+    use WithPagination;
+    public $cant = '10',
+        $cliente,
+        $campo = 'nombre_apellido',
+        $direccion = 'asc',
+        $buscar;
+
+    public function updatingBuscar()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
-        return view('livewire.tabla-clientes');
+        $clientes = t_clientes::where('id' , 'like', '%' . $this->buscar . '%')
+                            ->orWhere('dni' , 'like', '%' . $this->buscar . '%')
+                            ->orWhere('nombre_apellido' , 'like', '%' . $this->buscar . '%')
+                            ->orWhere('localidad' , 'like', '%' . $this->buscar . '%')
+                            ->orWhere('provincia' , 'like', '%' . $this->buscar . '%')
+                            ->orderBy($this->campo,$this->direccion)
+                            ->paginate($this->cant);
+        return view('livewire.tabla-clientes',compact('clientes'));
+    }
+
+    public function orden($campo){
+        if ($this->campo == $campo) {
+            if ($this->direccion == 'asc'){
+                $this->direccion = 'desc';
+            }
+            else{
+                $this->direccion = 'asc';
+            }
+        } else {
+            $this->campo = $campo;
+            $this->direccion = 'asc';
+        }
     }
 }
--- resources/views/livewire/tabla-clientes.blade.php
+++ resources/views/livewire/tabla-clientes.blade.php
@@ -19,8 +19,143 @@
 
             {{-- input para buscar --}}
             <x-jet-input type="text" class='flex-1 mx-4' placeholder="Escriba su busqueda..." wire:model="buscar" />
+            {{$buscar}}
 
         </div>
-        hola
+        @if (count($clientes))
+
+            <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col"
+                            class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                            wire:click="orden('id')">
+                            Id
+
+                            {{-- Icono de direccionamiento --}}
+                            @if ($campo == 'id')
+                                @if ($direccion == 'asc')
+                                    <i class="fas fa-sort-alpha-up-alt float-right"></i>
+                                @else
+                                    <i class="fas fa-sort-alpha-down-alt float-right"></i>
+                                @endif
+                            @else
+                                <i class="fas fa-sort float-right"></i>
+                            @endif
+
+                        </th>
+                        <th scope="col"
+                            class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                            wire:click="orden('dni')">
+                            DNI
+                            {{-- Icono de direccionamiento --}}
+                            @if ($campo == 'dni')
+                                @if ($direccion == 'asc')
+                                    <i class="fas fa-sort-alpha-up-alt float-right"></i>
+                                @else
+                                    <i class="fas fa-sort-alpha-down-alt float-right"></i>
+                                @endif
+                            @else
+                                <i class="fas fa-sort float-right"></i>
+                            @endif
+                        </th>
+                        <th scope="col"
+                            class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                            wire:click="orden('nombre_apellido')">
+                            Nombre y Apellido
+                            {{-- Icono de direccionamiento --}}
+                            @if ($campo == 'nombre_apellido')
+                                @if ($direccion == 'asc')
+                                    <i class="fas fa-sort-alpha-up-alt float-right"></i>
+                                @else
+                                    <i class="fas fa-sort-alpha-down-alt float-right"></i>
+                                @endif
+                            @else
+                                <i class="fas fa-sort float-right"></i>
+                            @endif
+                        </th>
+                        <th scope="col"
+                            class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                            wire:click="orden('localidad')">
+                            Localidad
+                            {{-- Icono de direccionamiento --}}
+                            @if ($campo == 'localidad')
+                                @if ($direccion == 'asc')
+                                    <i class="fas fa-sort-alpha-up-alt float-right"></i>
+                                @else
+                                    <i class="fas fa-sort-alpha-down-alt float-right"></i>
+                                @endif
+                            @else
+                                <i class="fas fa-sort float-right"></i>
+                            @endif
+                        </th>
+                        <th scope="col"
+                            class="cursor-pointer px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                            wire:click="orden('provincia')">
+                            Provincia
+                            {{-- Icono de direccionamiento --}}
+                            @if ($campo == 'provincia')
+                                @if ($direccion == 'asc')
+                                    <i class="fas fa-sort-alpha-up-alt float-right"></i>
+                                @else
+                                    <i class="fas fa-sort-alpha-down-alt float-right"></i>
+                                @endif
+                            @else
+                                <i class="fas fa-sort float-right"></i>
+                            @endif
+                        </th>
+                        
+                        <th scope="col" class="relative px-6 py-3">
+                            <span class="sr-only">Edit</span>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach ($clientes as $cli)
+                        <tr>
+                            <td class="px-6 py-4 ">
+                                <div class="text-sm text-gray-900">{{ $cli->id }}</div>
+                            </td>
+                            <td class="px-6 py-4 ">
+                                <div class="text-sm text-gray-900">{{ $cli->dni }}</div>
+                            </td>
+                            <td class="px-6 py-4 ">
+                                <div class="text-sm text-gray-900">{{ $cli->nombre_apellido }}</div>
+                            </td>
+                            <td class="px-6 py-4  text-sm text-gray-500">
+                                <div class="text-sm text-gray-900">{{ $cli->localidad }}</div>
+                            </td>
+                            <td class="px-6 py-4  text-sm text-gray-500">
+                                <div class="text-sm text-gray-900">{{ $cli->provincia }}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium flex">
+                                {{-- @livewire('editar-cliente', ['cliente' => $cliente], key($cliente->id)) --}}
+                                {{-- <a wire:click="editar({{ $cli }})"
+                                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+
+                                <a wire:click="$emit('borrarArticulo',{{ $cli->id }})"
+                                    class="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full cursor-pointer">
+                                    <i class="fas fa-trash"></i>
+                                </a> --}}
+                            </td>
+                        </tr>
+                    @endforeach
+                    <!-- More people... -->
+                </tbody>
+            </table>
+
+            @if ($clientes->hasPages())
+                <div class="px-6 py-3">
+                    {{ $clientes->links() }}
+                </div>
+            @endif
+        @else
+            <div class="px-6 py-4">
+                No existe ningun cliente
+            </div>
+
+        @endif
     </x-tabla>
 </div>
